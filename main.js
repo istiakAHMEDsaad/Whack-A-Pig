7ad63c1f15f9dbf5e839a0b2362f706c1c7161bf
Saad <100187174+istiakAHMEDsaad@users.noreply.github.com>
2024-03-12 15:37:10 +0600

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function run(){
 
     const img = document.createElement('img')
     img.classList.add('mole')
-    img.src = '/assets/pig-face.png'
+    img.src = 'assets/pig-face.png'
 
     img.addEventListener('click', () => {
         score += 10
